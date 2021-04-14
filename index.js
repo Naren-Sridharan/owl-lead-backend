45e11494d67b5b55639b1bca4b45adf967f3a021
Vaibhavm15 <54697878+Vaibhavm15@users.noreply.github.com>
2021-04-14 14:05:23 +1000

    installed dependencies and initialised the server locally for development

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-const express = require('express')
-
+// getting the express server
+const express = require('express');
+const bodyParser = require("body-parser");
+// getting routes for anyone around
+// const sensorRoutes = require("./anyone_around/routes/sensorRouter")
+// getting express instance
 const app = express();
 
+// defining the port
 const PORT = process.env.PORT || 3000;
 
+// initializing the express server
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
 });
+
+// parse requests of content-type: application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type: application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// create sensor routes
+// app.use('/sensors' , sensorRoutes);
+
+//defining root route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to VynlX application." });
+});
+
+
+
 
 app.get('/health_check', (request, response) => {
     var package_json = require('./package.json');
